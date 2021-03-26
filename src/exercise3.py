7ca0e1ba583a6f875ca8580f5cf39c445fe2f151
group7BCS1 <2020bcs036@std.must.ac.ug>
2021-03-26 09:41:34 +0300

    modified src

---
+++ src/exercise3.py
@@ -0,0 +1,9 @@
+#Hours worked and tme taken
+
+prompt_hours = input('Enter hours; ')
+prompt_time = input('Enter time; ')
+result = float(prompt_hours) * float(prompt_time)
+
+#Gross pay
+gross_pay = ('pay;'+ str(result))
+print(gross_pay)
