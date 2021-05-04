2e2034679fe616f48baa68b1777697e6981cdfc5
group7BCS1 <2020bcs036@std.must.ac.ug>
2021-05-04 16:55:13 +0300

    90% done

--- src/mid_term/project.py
+++ src/mid_term/project.py
@@ -5,16 +5,48 @@ def gallons_used(x, y):
     else:
         x = 1000000000 - x
         gallons = (x + y) / 10
-    print("Gallons of water used: ", round(gallons, 2))
+    print(f"Gallons of water used:{gallons:.2f}")
     return gallons
 
 
 while True:
     customer_code = input("Enter Customer code: ")
-    beginning_reading = int(input("Enter beginning reading"))
-    ending_reading = int(input("Enter ending reading"))
-    print(f"Customer code:{customer_code}")
-    print(f"Beginning meter reading:{beginning_reading:09d}")
-    print(f"Ending meter reading:{ending_reading:09d}")
-    gallons = gallons_used(beginning_reading, ending_reading)
+    if customer_code == 'r' or customer_code == 'R':
+        beginning_reading = int(input("Enter beginning reading"))
+        ending_reading = int(input("Enter ending reading"))
+        print(f"Customer code: {customer_code}")
+        print(f"Beginning meter reading: {beginning_reading:09d}")
+        print(f"Ending meter reading: {ending_reading:09d}")
+        gallons = gallons_used(beginning_reading, ending_reading)
+        bill = 5 + (gallons * 0.0005)
+        print(f"Amount billed: ${bill:.2f}")
+    elif customer_code == 'c' or customer_code == 'C':
+        beginning_reading = int(input("Enter beginning reading"))
+        ending_reading = int(input("Enter ending reading"))
+        print(f"Customer code:{customer_code}")
+        print(f"Beginning meter reading:{beginning_reading:09d}")
+        print(f"Ending meter reading:{ending_reading:09d}")
+        gallons = gallons_used(beginning_reading, ending_reading)
+        if gallons <= 4000000:
+            bill = 1000
+        else:
+            bill = 1000 + ((gallons - 4000000) * 0.00025)
+        print(f"Amount billed:${bill:.2f}")
+    elif customer_code == 'i' or customer_code == 'I':
+        beginning_reading = int(input("Enter beginning reading"))
+        ending_reading = int(input("Enter ending reading"))
+        print(f"Customer code:{customer_code}")
+        print(f"Beginning meter reading:{beginning_reading:09d}")
+        print(f"Ending meter reading:{ending_reading:09d}")
+        gallons = gallons_used(beginning_reading, ending_reading)
+        if gallons <= 4000000:
+            bill = 1000
+        elif 4000000 < gallons <= 10000000:
+            bill = 2000
+        else:
+            bill = 2000 + (gallons - 10000000) * 0.00025
+        print(f"Amount billed: ${bill:.2f}")
+    else:
+        print("Invalid Entry")
+        break
 
