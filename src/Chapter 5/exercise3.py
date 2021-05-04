7a4b21e2778be6bef091748655934d71eeab6bb6
group7BCS1 <2020bcs036@std.must.ac.ug>
2021-05-04 23:46:37 +0300

    95% done

--- src/Chapter 5/exercise3.py
+++ src/Chapter 5/exercise3.py
@@ -1,27 +1,71 @@
 print('\nWelcome to the vending machine change maker program')
 print('\nChange maker initialized')
-print('\nStock contians:')
+print('\nStock contains:')
 nickel = 25
 dime = 25
 quarter = 25
 one = 0
 five = 0
-print( '',nickel ,' Nickels\n',dime,'Dimes\n',quarter,'quarters\n',one,'Ones\n',five,'Fives\n')
+print('', nickel, ' Nickels\n', dime, 'Dimes\n', quarter, 'quarters\n', one, 'Ones\n', five, 'Fives\n')
 
 while True:
     try:
-        user_input=input("enter the purchase price(xx.xx) or 'q' to quite:\n ")
+        user_input = input("enter the purchase price(xx.xx) or 'q' to quite:\n ")
         if user_input == 'q':
             break
-        user_input=float(user_input)
-        user_input=((user_input*100)%5==0)
-        if user_input>0 and ((user_input*100)%5==0):
-            user_input=int(round(((user_input*100)%5==0),2))
-            print('\ndeposit menu:\n')
-            print("'n'-deposit a nickel\n'd'-deposit a dime\n'q'-depost a qaurter\n'o'-deposit a one dollar bill\n'f'-deposit a five dollar bill\n'c'-cancel the purchase\n")
-            
-        else:
-            print('invalid input')
-        print(user_input)
-    except:
-        print('invalid input')
+        user_input = float(user_input)
+        user_input = ((user_input * 100) % 5 == 0)
+        if user_input > 0 and ((user_input * 100) % 5 == 0):
+            user_input = int(round(((user_input * 100) % 5 == 0), 2))
+            print("\ndeposit menu:\nMenu for deposits:\n 'n' - deposit a nickel\n 'd' - deposit a dime",\
+        "\n 'q' -deposit a quarter\n 'f' -deposit a five dollar bill",\
+        "\n 'c' -cancel the purchase")
+        print("\nPayment due: ", dollars, "dollars and", cents, "cents")
+        deposit_str = input("indicate your deposit: ")
+        while user_input > 0:
+            if not deposit_str.isdigit():
+                if deposit_str =='n':
+                    user_input -= 5
+                    nickels += 1
+                    dollars = user_input // 100
+                    cents = user_input - (dollars *100)
+                elif deposit_str == 'd':
+                    user_input -= 25
+                    quarters += 1
+                    dollars = total_cents //100
+                    cents = total_cents - (dollars * 100)
+                elif deposit_str == 'o':
+                    user_input -= 100
+                    ones += 1
+                    dollars = total_cents // 100
+                    cents = total_cents - (dollars * 100)
+                elif deposit_str =='f':
+                    user_input -= 500
+                    fives += 1
+                    dollars = total_cents -(dollars * 100)
+                elif deposit_str == 'c':
+                    break
+                    else:
+        print("Illegal deposit: ", deposit_str)
+            if user_input > 0:
+                print("\nPayment due: ", dollars, "dollars and", cents, "cents")
+                deposit_str = input('Indicate your deposit: ')
+            else:
+                if user_input == 0:
+                    print("\nPlease take the change below.\n No change due.")
+                    break
+                change_quarters = abs(user_input) // 25
+                if change_quarters > quarters:
+                    change_quarters = quarters
+
+
+
+
+
+
+
+
+
+
+
+
--- src/mid_term/project.py
+++ src/mid_term/project.py
@@ -1,8 +1,10 @@
+# PROGRAM COMPUTING AND DISPLAYING INFORMATION FOR A UTILITY COMPANY THAT SUPPLIES WATER
 # function to calculate the number of gallons used
 def gallons_used(x, y):
     if y > x:
         gallons = (y - x) / 10
     else:
+        # incase the beginning reading is bigger than the ending reading
         x = 1000000000 - x
         gallons = (x + y) / 10
     print(f"Gallons of water used:{gallons:.2f}")
@@ -11,16 +13,21 @@ def gallons_used(x, y):
 
 while True:
     customer_code = input("Enter Customer code: ")
+    # r is the code for residential
     if customer_code == 'r' or customer_code == 'R':
         beginning_reading = int(input("Enter beginning reading"))
         ending_reading = int(input("Enter ending reading"))
         print(f"Customer code: {customer_code}")
+        # adding 09d because if a single digit is inserted, the leading zeros are added
         print(f"Beginning meter reading: {beginning_reading:09d}")
         print(f"Ending meter reading: {ending_reading:09d}")
+        # replacing x and y with the beginning and ending readings respectively
         gallons = gallons_used(beginning_reading, ending_reading)
         bill = 5 + (gallons * 0.0005)
-        print(f"Amount billed: ${bill:.2f}")
+        # formatting the amount to print the value with 2 fractional digits
+        print(f'Amount billed: ${bill:.2f}')
     elif customer_code == 'c' or customer_code == 'C':
+        # c is the code for commercial
         beginning_reading = int(input("Enter beginning reading"))
         ending_reading = int(input("Enter ending reading"))
         print(f"Customer code:{customer_code}")
@@ -33,6 +40,7 @@ while True:
             bill = 1000 + ((gallons - 4000000) * 0.00025)
         print(f"Amount billed:${bill:.2f}")
     elif customer_code == 'i' or customer_code == 'I':
+        # i is the code for industrial
         beginning_reading = int(input("Enter beginning reading"))
         ending_reading = int(input("Enter ending reading"))
         print(f"Customer code:{customer_code}")
@@ -49,4 +57,3 @@ while True:
     else:
         print("Invalid Entry")
         break
-
